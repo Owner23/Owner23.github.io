81da3f3763f78c22f6746c30ba54a74277397e1b
Owner23 <58201458+Owner23@users.noreply.github.com>
2020-01-10 21:27:32 -0800

    Added a little disclaimer
    I don't mind countryhumans. Please note that I've had apokalypto's design on my mind long before I knew they were a thing.

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-console.log('Hello, what\'s this? https://www.youtube.com/channel/UCJDFINJB23y0MeXO6BX7EFw?sub_confirmation=1');
+console.log('I don\'t mean to bash countryhumans btw. I Just wanted to point out that I didn\'t draw inspiration from them when it came to designing apokalypto. While you\'re here, why don\'t you check this out? I promise it\'ll bee good! https://www.youtube.com/channel/UCJDFINJB23y0MeXO6BX7EFw?sub_confirmation=1');
 
